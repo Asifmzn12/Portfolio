624c906ca5250c6d0db77a414fab77a6ff49686f
Asif Ali <asifmzn13@gmail.com>
2023-10-18 11:04:47 +0530

    some changes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,79 +1,36 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-// import {PiCodeSimpleFill} from 'react-icons/pi'
-import images from './config/experiencecustomapi';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import experienceData from './config/experiencecustomapi';
+
+
+
 function Experience() {
   return (
-
     <div className="container py-md-5 py-3">
-     <h2 className="text-center fw-bold mb-5">Experience</h2>
-    <div className='Stepper'>
-    <VerticalTimeline className='w-100 mw-100' layout="1-column-left">
-    <VerticalTimelineElement
-      className="vertical-timeline-element--work"
-      contentStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-      contentArrowStyle={{ borderRight: '7px solid  rgb(255 255 255)' }}
-      date="August 2023 - present"
-      iconStyle={{ background: 'rgba(255, 255, 255,1)', color: '#fff' }}
-   icon={<img src={images.Developer} className='img-fluid' alt="Experience" />}
-  >
-      <h3 className="vertical-timeline-element-title">Software Developer</h3>
-      <h5 className="vertical-timeline-element-subtitle my-2">Plaxonic Inc</h5>
-      <p>
-      Role & Reponsibilites: Web Developepment, Creating Intractiive and Responsive UI
-      </p>
-    </VerticalTimelineElement>
-    <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(255 255 255)' }}
-    date="January 2023 - May 2023"
-    iconStyle={{ background: 'rgba(255, 255, 255,1)', color: '#fff' }}
-     icon={<img src={images.Developer} className='img-fluid' alt="Experience" />}
-
-    >
-    <h3 className="vertical-timeline-element-title">Software Developer</h3>
-    <h5 className="vertical-timeline-element-subtitle my-2">Redian  Software </h5>
-    <p>
-    Role & Reponsibilites: Full time Frontend Developer 
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-  className="vertical-timeline-element--work"
-  contentStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-      contentArrowStyle={{ borderRight: '7px solid  rgb(255 255 255)' }}
-      date="Nom 2022 - December 2022"
-      iconStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-      icon={<img src={images.Developer} className='img-fluid' alt="Experience" />}
-      >
-      <h3 className="vertical-timeline-element-title">Software Developer Intern</h3>
-      <h5 className="vertical-timeline-element-subtitle my-2">Redian Software</h5>
-      <p>
-      Role & Reponsibilites: Frontend Developer Intern
-      </p>
-</VerticalTimelineElement>
-<VerticalTimelineElement
-className="vertical-timeline-element--work"
-contentStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-contentArrowStyle={{ borderRight: '7px solid  rgb(255 255 255)' }}
-date="Febuary 2022 - Aug 2022"
-iconStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
-icon={<img src={images.Developer} className='img-fluid' alt="Experience" />}
->
-<h3 className="vertical-timeline-element-title">Apprenticeship</h3>
-<h5 className="vertical-timeline-element-subtitle my-2">Ducat Training Center </h5>
-<p>
-Role & Reponsibilites: Get Traning for Mern Stack
-</p>
-</VerticalTimelineElement>
-
-    </VerticalTimeline>
-   
+      <h2 className="text-center fw-bold mb-5">Experience</h2>
+      <div className='Stepper'>
+        <VerticalTimeline className='w-100 mw-100' layout="1-column-left">
+          {experienceData.map((experience, index) => (
+            <VerticalTimelineElement
+              key={index}
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: 'rgba(20, 20, 58,1)', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  rgb(255 255 255)' }}
+              date={experience.date}
+              iconStyle={{ background: 'rgba(255, 255, 255,1)', color: '#fff' }}
+              icon={<img src={experience.img} className='img-fluid' alt="Experience" />}
+            >
+              <h3 className="vertical-timeline-element-title">{experience.title}</h3>
+              <h5 className="vertical-timeline-element-subtitle my-2">{experience.company}</h5>
+              <p>
+                Role & Responsibilities: {experience.role}
+              </p>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
     </div>
-
-      
-    </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/config/experiencecustomapi.js
+++ src/config/experiencecustomapi.js
@@ -1,9 +1,31 @@
-const images={
-    Developer:'./image/developer.png',
-    coding:'./image/code icons.png',
-    codeicon:'./image/codingicon.png',
-    experience:'./image/experience icon.png'
-
-
-}
-export default images
+const experienceData = [
+    {
+      img:'./image/developer.png',    
+      title: 'Software Developer',
+      company: 'Plaxonic Inc',
+      date: 'August 2023 - present',
+      role: "Interactive Web Developer, Dedicated to Crafting Engaging and Responsive User Interfaces for Enhanced Online Interactions.",
+    },
+    {
+      img:'./image/code icons.png',
+      title: 'Software Developer',
+      company: 'Redian Software',
+      date: 'January 2023 - May 2023',
+      role: 'Frontend Web Developer, Proficient in Building Interactive and User-Friendly Interfaces, Focused on Responsive Design.',
+    },
+    {
+      img:'./image/codingicon.png',
+      title: 'Software Developer Intern',
+      company: 'Redian Software',
+      date: 'Nov 2022 - December 2022',
+      role: 'Junior Frontend Developer Intern, Gaining Hands-on Experience in Building User-Friendly Interfaces and Web Development Technologies.',
+    },
+    {
+        img:'./image/developer.png',    
+        title: 'Apprenticeship',
+      company: 'Ducat Training Center',
+      date: 'Feb 2022 - Aug 2022',
+      role: 'MERN Stack Learner, Embracing the Journey of Learning MongoDB, Express.js, React, and Node.js to Build Modern and Scalable Web Applications.',
+    },
+  ];
+  export default experienceData;
